442593aff0489f1906ef66553085796e26018731
Tran Minh Tai <tranminhtai1998@gmail.com>
2020-10-07 13:47:09 +0700

    Basic Array JS

---
+++ javascript/trycode/concat.js
@@ -0,0 +1,5 @@
+function run(a, b) {
+  c = b.concat(a);
+  console.log(c);
+}
+run(["a", "b", "c"], [1, 2, 3]);
---
+++ javascript/trycode/main.js
@@ -0,0 +1,12 @@
+function run() {
+  var data = [25, 154, 461, 67, 32, 43]; // define an array
+
+  var length = data.length; // get length of array datas
+
+  console.log(data.length); // output: 6
+
+  data.push(length); // Add new element to datas
+
+  console.log(data.length); // output: 7
+}
+run();
